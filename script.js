4bae1a05e8bfeea54cce2177fa4abb680c34de83
Zuha22 <121706741+Zuha22@users.noreply.github.com>
2025-12-05 09:24:07 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let cart = JSON.parse(localStorage.getItem("cart") || "[]");
 
 // Add item to cart
-function AddToCart(name, price) {
+function addToCart(name, price) {
   cart.push({ name, price });
   localStorage.setItem("cart", JSON.stringify(cart));
   alert(name + " added to cart");
